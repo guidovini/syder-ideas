2e76544bc7f1f61276ba9539f97bbb5d29b90b6f
Guido Santillan <guidosantillan01@gmail.com>
2019-01-23 18:23:38 -0500

    localStorage enabled

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import { createStore } from 'redux'
 import Root from 'Root'
 import reducers from 'reducers'
 
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+
 const store = createStore(
-  reducers, 
+  reducers,
+  persistedState, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
+
 
 ReactDOM.render(<Root store={store}/>, document.getElementById('app'))
