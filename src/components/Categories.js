03052732e78e957cbb67c7a5d1e86bd5b3780e19
Guido Santillan <guidosantillan01@gmail.com>
2019-01-24 18:28:10 -0500

    Categories, CategoryItem layout changed

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,53 +1,16 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React from 'react'
 
 import Header from 'components/Header'
 import Navigation from 'components/Navigation'
+import CategoryList from 'components/CategoryList'
 
-const FilterCategories = ({ ideas }) => {
-  let categories = new Set([])
-  ideas.forEach(idea => categories.add(idea.category))
-  categories = [...categories]
-
+export default function Categories() {
   return (
     <div>
-      {categories.map(category => {
-        const conditions = ideas.filter(idea => idea.category === category)
-        return (
-          <div key={category}>
-            <h3>{category}</h3>
-            {conditions.map(condition => {
-              return (
-                <Link to={`/idea/${condition.id}`} key={condition.id}>
-                  <h4>{condition.name}</h4>
-                  <p>{condition.description}</p>
-                </Link>
-              )
-            })}
-          </div>
-        )
-      })}
+      <Header />
+      <Navigation />
+      <h2>Idea Categories</h2>
+      <CategoryList/>
     </div>
   )
-
 }
-
-export class Categories extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Navigation />
-        <h2>Idea Categories</h2>
-        <FilterCategories ideas={this.props.ideas}/>
-      </div>
-    )
-  }
-}
-
-const mapStateToProps = (state) => ({
-  ideas: state.ideas
-})
-
-export default connect(mapStateToProps)(Categories)
