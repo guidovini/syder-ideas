d5fb224625c9d683be77dd6300796c2697506013
Guido Santillan <guidosantillan01@gmail.com>
2019-01-30 11:59:24 -0500

    server.js changed to prepare for heroku deployment

--- server/server.js
+++ server/server.js
@@ -3,45 +3,51 @@ require('dotenv').config();
 const { Client } = require('pg');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'client/build');
+const port = process.env.PORT || 5000;
 const app = express();
 
-const port = process.env.PORT || 5000;
+// Serve static files from the React app
+app.use(express.static(publicPath));
 
 // app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the React app
-app.use(express.static(__dirname + "/public"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
 
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-});
 
-client.connect();
+// const client = new Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+// });
 
-app.get("/api/ideas", function(req, res) {
-  const query = 'SELECT * FROM ideas';
-  client.query(query, (err, results) => {
-    if (err) throw err;
-    const ideas = results.rows;
-    res.send({ ideas })
-    // res.render("home", {data: count});
-  });
-});
+// client.connect();
 
-app.get("/api/create", function(req, res) {
-  const query = 'INSERT INTO ideas(category, name, description, target, user_id) VALUES ($1, $2, $3, $4, $5)'
-  const values = ['Web app1', 'Syder Ideas1', 'A solution for people who wants to store their ideas1', 'People who wants to store their ideas1', 1];
-  client.query(query, values, (err, result) => {
-    if (err) throw err;
-    res.redirect("/api/ideas");
-  });
-});
+// app.get("/api/ideas", function(req, res) {
+//   const query = 'SELECT * FROM ideas';
+//   client.query(query, (err, results) => {
+//     if (err) throw err;
+//     const ideas = results.rows;
+//     res.send({ ideas })
+//     // res.render("home", {data: count});
+//   });
+// });
+
+// app.get("/api/create", function(req, res) {
+//   const query = 'INSERT INTO ideas(category, name, description, target, user_id) VALUES ($1, $2, $3, $4, $5)'
+//   const values = ['Web app1', 'Syder Ideas1', 'A solution for people who wants to store their ideas1', 'People who wants to store their ideas1', 1];
+//   client.query(query, values, (err, result) => {
+//     if (err) throw err;
+//     res.redirect("/api/ideas");
+//   });
+// });
 
 // app.get('/api/hello', (req, res) => {
 //   res.send({ express: 'Hello From Express' });
