0adfc33c9bd5175121ea34e15c30f387f102e3af
Guido Santillan <guidosantillan01@gmail.com>
2019-01-31 11:12:15 -0500

    client checkpoint

--- client/server/server.js
+++ client/server/server.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 5000;
 const app = express();
 
-// // Serve static files from the React app
-app.use(express.static(publicPath));
-
-// app.set("view engine", "ejs");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(publicPath, 'static')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
