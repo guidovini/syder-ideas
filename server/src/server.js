aa944155366fc385ddbbee19469421fbcbb2b443
Guido Santillan <guidosantillan01@gmail.com>
2019-02-05 13:57:59 -0500

    Added pooling to server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,35 +8,23 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
-// const whitelist = ['http://syder-ideas-client.herokuapp.com'];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-
 app.use(cors());
 app.set("views", __dirname + '/views')
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const client = new Client({
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_NAME,
-  // password: process.env.DB_PASS,
-  // port: process.env.DB_PORT,
-  connectionString: process.env.DATABASE_URL || '',
-  ssl: true
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  // connectionString: process.env.DATABASE_URL,
+  // ssl: true
 });
 
-client.connect();
+pool.connect();
 
 app.get("/", (req, res) => {
   res.send('This is the main page. Go to /api/ideas to gather data and to /api/form to add a new idea')
@@ -44,7 +32,8 @@ app.get("/", (req, res) => {
 
 app.get("/api/ideas", (req, res) => {
   const query = 'SELECT * FROM ideas';
-  client.query(query, (err, results) => {
+
+  pool.query(query, (err, results) => {
     if (err) throw err;
     const ideas = results.rows;
     res.send({ ideas });
@@ -56,10 +45,11 @@ app.get("/api/form", (req, res) => {
 })
 
 app.post("/create", (req, res) => {
-  const query = 'INSERT INTO ideas(category, name, description, target, user_id) VALUES ($1, $2, $3, $4, $5)'
-  const { category, name, description, target } = req.body
+  const query = 'INSERT INTO ideas(category, name, description, target, user_id) VALUES ($1, $2, $3, $4, $5)';
+  const { category, name, description, target } = req.body;
   const values = [category, name, description, target, 1];
-  client.query(query, values, (err, result) => {
+
+  pool.query(query, values, (err, result) => {
     if (err) throw err;
     res.redirect("/api/ideas");
   });
