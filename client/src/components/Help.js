abdbae0673546f3d944c91949b6f85258ac4f475
Guido Santillan <guidosantillan01@gmail.com>
2019-02-28 11:01:43 -0500

    Add Help and Profile pages

---
+++ client/src/components/Help.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Help = () => {
+  return (
+    <div className="section">
+      <div className="columns is-centered">
+        <div className="column is-three-fifths">
+            <h1 className="title is-4">Help</h1>
+            <p>This is the help section</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Help
