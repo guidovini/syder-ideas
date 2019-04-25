feaee76df3cb9300227569deab57b65ad39d5a61
Guido Santillan <guidosantillan01@gmail.com>
2019-04-25 10:20:48 -0500

    Add /test route

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -5,20 +5,20 @@
 const express = require('express');
 const router = express.Router();
 
-const passportService = require('../services/passport'); // ! DO NOT DELETE THIS LINE
-const passport = require('passport');
-
 const authController = require('../controllers/auth');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
+const passportService = require('../services/passport'); // ? DO NOT DELETE THIS LINE
+const passport = require('passport');
 const requireLogin = passport.authenticate('local', { session: false });
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 //
 // ─── AUTH ROUTER ────────────────────────────────────────────────────────────────
 //
 
-router.get('/blocked-access', requireAuth, authController.blocked_access);
 router.post('/signup', authController.signup);
 router.post('/login', requireLogin, authController.login);
+
+router.get('/test', requireAuth, authController.test);
 
 module.exports = router;
