cd18677db8110bc58cb8df389b0b3c32ce56c2a5
Guido Santillan <guidosantillan01@gmail.com>
2019-05-28 10:54:22 -0500

    Add auth, strategies, inspiration reducers

--- client/src/reducers/app.js
+++ client/src/reducers/app.js
@@ -1,15 +1,19 @@
 import { combineReducers } from 'redux'
 
-import ideasReducer from 'reducers/ideas'
-import menuReducer from 'reducers/menu'
-import featuresReducer from 'reducers/features'
-import filtersReducer from 'reducers/filters'
-import authReducer from 'reducers/auth'
+import ideasReducer from './ideas'
+import menuReducer from './menu'
+import featuresReducer from './features'
+import filtersReducer from './filters'
+import authReducer from './auth'
+import strategiesReducer from './strategies'
+import inspirationReducer from './inspiration'
 
 export default combineReducers({
   ideas: ideasReducer,
   menu: menuReducer,
   features: featuresReducer,
   filters: filtersReducer,
-  auth: authReducer
+  auth: authReducer,
+  strategies: strategiesReducer,
+  inspiration: inspirationReducer
 })
